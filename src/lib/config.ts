d212f0a527accb3bd834af0045a8148c93629520
Henrik Sommerfeld <henrik@sommerfeld.nu>
2025-07-08 19:21:46 +0200

    Update public url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'Recept'
 export const description = 'Isabels och Henriks receptsamling'
-export const url = dev ? 'http://localhost:5173' : 'https://recept.netlify.com'
+export const url = dev ? 'http://localhost:5173' : 'https://recept.sommerfeld.nu'
