7312e177b689d0f9ee562030afdb5ce31bb3814b
Henrik Sommerfeld <henrik@sommerfeld.nu>
2025-09-21 08:43:38 +0200

    Migrate to /state

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,11 @@
 <script>
   import * as config from '$lib/config'
-  import { page } from '$app/stores'
+  import { page } from '$app/state'
 </script>
 
 <svelte:head>
   <title>{config.title}</title>
 </svelte:head>
 
-<h1>{$page.status}</h1>
-<h2>{$page.error?.message}</h2>
+<h1>{page.status}</h1>
+<h2>{page.error?.message}</h2>
